6d6619aafd2e19b828ef87727afa0d7965241757
Jan Gassen <jan.gassen@centerdevice.com>
2014-12-21 16:42:11 +0100

    Initial Commit

---
+++ ArtlessEdit/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ArtlessEdit
+//
+//  Created by Jan Gassen on 21/12/14.
+//  Copyright (c) 2014 Jan Gassen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
